252dbc706a24cbf96d108d4456c491a5297ddce9
Venc03 <vegi.dani1@gmail.com>
2023-03-02 13:18:29 +0100

    elso resz

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,7 +1,17 @@
 package ciklusok;
 public class Szamok {
     public static void main(String[] args) {
-        
+        kiiras1();
+    }
+
+    private static void kiiras1() {
+        for (int r = 2; r <= 3; r++){
+            for (int x = 2; x <= 4; x++){
+                for (int i = 2; i <= 4; i++){
+                    System.out.println(r + "" + x + "" + i);
+                }
+            }
+        }
     }
     
 }
